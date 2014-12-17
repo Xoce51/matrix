127579963008dbc90b3af6a184aa14fae9b0887c
Aubin Gerard <aubin.gerard@gmail.com>
2014-12-17 16:30:38 +0100

    Finish js display, Todo send data with ajax

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 "use strict";
 
 jQuery(function($){
+	
+	// initial var
 	var $choice = $("input[name=choice]");
 	var totalNumber = 0;
 	var matrixSize = {
@@ -15,16 +17,18 @@ jQuery(function($){
 		}
 	};
 	
+	// display matrix size tab
 	$choice.on("click", function(e){
 		$('.matrix.first').fadeIn();
 	});
 	
+	// Check matrix value and set the array if value are ok
 	$(".matrix_value").on("blur", function(e){
 		totalNumber++;
 		var $dom = $(e.target);
 		if ($dom.val() > 0 && $dom.val() <= 6)
 		{
-			matrixSize.push($dom.val());
+			matrixSize[$dom.attr("data-matrix")][$dom.attr("data-matrixType")] = $dom.val();
 		}
 		else
 		{
@@ -32,9 +36,44 @@ jQuery(function($){
 			alert('invalid Number please enter a number between 1 and 5');
 		}
 		
-		console.log(matrixSize);
-		
-		console.log($dom.val());
 	});
-	
+
+	// Check if matrix number are OK and display the array
+	$(document).on("click", "#display_matrix", function(e){
+			console.log(matrixSize);
+			var $res = $("#res");
+			var html = '<table>';
+			var i = 0;
+			var j = 0;
+
+			// fill first tab...
+			for(i; i < matrixSize.a.y; i++)
+			{
+				html += '<tr>';
+				for(j; j < matrixSize.a.x; j++)
+				{
+					html += '<td><input type="text" placeholder="X" name="a_'+ j +'_' + i + '" /></td>';
+				}
+				j = 0;
+				html += "</tr>";
+			}
+			html += '</table>';
+			$res.find(".first").append(html);
+			
+			// second array
+			html = '<table>';
+			for(i = 0; i < matrixSize.b.y; i++)
+			{
+				html += '<tr>';
+				for(j = 0; j < matrixSize.b.x; j++)
+				{
+					html += '<td><input type="text" placeholder="X" name="b_'+ j +'_' + i + '" /></td>';
+				}
+				j = 0;
+				html += "</tr>";
+			}
+			$(".matrix.second").fadeIn();
+			// display and append html content
+			$res.fadeIn().find(".second").append(html);
+		});
 });
