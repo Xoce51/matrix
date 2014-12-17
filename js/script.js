cd60b4275b32ad5475ce536c3815207a09121e95
Aubin Gerard <aubin.gerard@gmail.com>
2014-12-17 15:48:08 +0100

    Update html and js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,38 @@
 
 jQuery(function($){
 	var $choice = $("input[name=choice]");
+	var totalNumber = 0;
+	var matrixSize = {
+		a: {
+			x: 1,
+			y: 1
+		},
+		b: {
+			x: 1,
+			y: 1
+		}
+	};
 	
 	$choice.on("click", function(e){
 		$('.matrix.first').fadeIn();
 	});
-	console.log($choice);
+	
+	$(".matrix_value").on("blur", function(e){
+		totalNumber++;
+		var $dom = $(e.target);
+		if ($dom.val() > 0 && $dom.val() <= 6)
+		{
+			matrixSize.push($dom.val());
+		}
+		else
+		{
+			$dom.val(1);
+			alert('invalid Number please enter a number between 1 and 5');
+		}
+		
+		console.log(matrixSize);
+		
+		console.log($dom.val());
+	});
+	
 });
