7d52a69935f51081bb9d7fd5f258dca8197bb97d
Aubin Gerard <aubin.gerard@gmail.com>
2014-12-17 15:34:54 +0100

    Update adding js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-"use strict"
-jQuery(function($){
 
+"use strict";
+
+jQuery(function($){
+	var $choice = $("input[name=choice]");
+	
+	$choice.on("click", function(e){
+		$('.matrix.first').fadeIn();
+	});
+	console.log($choice);
 });
